256bb2fa58d988a51c20030683eb6d4e827509ad
Sixten <Siggbo@Buyns-MacBook-Pro.local>
2022-11-14 15:07:06 +0100

    texts

--- DEMOS/demo.py
+++ DEMOS/demo.py
@@ -4,7 +4,7 @@ from demoPlayer import *
 
 import random
 
-scene = SEAS.newScene('MainScene', True)
+scene = SEAS.addScene('MainScene', frameLimit=60, isTargeted=True)
 
 # Objects
 playerCoords = [ [100, 100], [120, 100], [120, 150], [100, 170], [50, 64] ]
@@ -18,6 +18,9 @@ SEAS.addRawNameHitboxGroup('PlayerColl', ['Player'])
 SEAS.createHitboxGroup('test', True)
 SEAS.addRawNameHitboxGroup('test', ['Player'])
 
+# Font
+font1 = SEAS.getCoreModule('Font').createFont('SiggboFont')
+scene.addText(font=font1, textName='SiggboText', text='Hello, My name is sixten', color="#f0f0f0", position=[200, 200])
 
 # Mat
 SEAS.createMaterial('PlayerMat', "#d65d0e")
